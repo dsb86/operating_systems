c459b458cb70b98174b7ad1f7cd978fcf1aab883
Daniel Brotman <dsb86@case.edu>
2016-09-23 15:56:49 +1200

    began parsing

--- Assignment2/TinyDOS.py
+++ Assignment2/TinyDOS.py
@@ -2,29 +2,55 @@ import volume
 
 class TinyDOS:
 
-    def __init__(self, vdrive):
-        self.myvolume= volume.Volume(vdrive)
+    def __init__(self):
+        self.myvolume = None
 
     def main(self):
+        connection = input('')
+
+        divider = 0
+        while (connection[divider] != ' '):
+            divider += 1
+
+        function = connection[:divider]
+        arg = connection[divider+1:]
 
 
-    def format(self):
+
+        switch = {'format': self.format, 'reconnect': self.reconnect, any: self.fatal_error}
+
+        while(1):
+            data = input('')
+
+            divider = 0
+            while (data[divider] != ' '):
+                divider += 1
+
+            function = data[:divider]
+            arg = [data[:divider + 1:]]
+
+            switch[function](arg)
+
+
+
+    def format(self, *arg):
+        self.myvolume = volume.Volume(arg[0])
         self.myvolume.format()
 
-    def reconnect(self):
-        self.myvolume.format()
+    def reconnect(self, *arg):
+        self.myvolume = volume.Volume(arg[0])
+        self.myvolume.reconnect()
+
+    def fatal_error(self):
+        raise IOError('failed to connect to drive')
 
 
 
 
 
-if __name__== 'main':
-    connection = input()
-    divider = 0
-    while(connection[divider] != ' '):
-        divider += 1
 
-    function = connection[:divider]
-    arg = connection[:divider+1:]
+if __name__== '__main__':
 
-    switch
+
+    me = TinyDOS()
+    me.main()
